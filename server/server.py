1aa8ebc05f31ba853ed6dc7dc668bfc1dcd77a0a
Vlad <gladladvlad@yandex.com>
2018-04-29 18:58:23 +0300

    + added cookie support to RequestHandler

--- server/server.py
+++ server/server.py
@@ -2,7 +2,10 @@ import BaseHTTPServer
 import SocketServer
 import shutil
 import os
+import re
 from urlparse import parse_qs, urlparse
+
+#from lib.acx_cookie import *
 
 PORT = 8000
 
@@ -15,6 +18,50 @@ userDB = [
 ]
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def get_cookie (self, key):
+        requestHandler = self
+        #le-am definit pe asta ca... ca sa fie
+        affirmative = 'yes'
+        negative = 'no'
+
+        #extrag headeru' din request
+        header = requestHandler.headers
+        #ma asigur ca key-u' are sens
+        key = str(key)
+
+
+        #linia cu cookie-urile din header
+        cookies = header.getallmatchingheaders('Cookie')[0]
+        #separ stringu' cu cookieuri intr-un vector de forma <key=value>
+        cookies = cookies.strip().replace(' ', '').replace('Cookie:', '', 1).split(';')
+
+        for cookie in cookies:
+            # elimin key-u'
+            value = re.sub(''.join(['^', key, '=']), '', cookie, 1)
+
+            # daca intr-adevar am eliminat ceva
+            if value != cookie:
+                # si am ramas cu o valoare
+                if value != '':
+                    return value
+                else:
+                    return affirmative
+
+        return negative
+
+    def set_cookie (self, key, value='', other=''):
+        requestHandler = self
+        pair = []
+
+        pair.append(key)
+        if value != '':
+            pair.append('=')
+            pair.append(value)
+
+        pair = ''.join(pair)
+
+        requestHandler.send_header('Set-Cookie', pair)
+
 
     def do_GET(self):
 
@@ -29,12 +76,25 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            if creds in userDB:
-                self.wfile.write("SUCCESS")
-            else:
-                self.wfile.write("FAIL")
+            self.send_header("Set-Cookie", "tetest")
 
+            #read cookie
+            #if 'logged=yes' in self.rfile.read():
+                #print 'already LOGGED'
+
+            logged = self.get_cookie('logged')
+
+            if logged == 'yes':
+                print 'already logged as someone'
+
+            if creds in userDB:
+                # self.send_header("Set-Cookie", "logged=yes")
+                # same ting
+                self.set_cookie('logged', 'yes')
+            else:
+                self.send_header("Set-Cookie", "logged=no")
+
+            self.end_headers()
             return
 
         try:
@@ -69,9 +129,6 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
 
             self.end_headers()
-
-            self.wfile.write("Error")
-
 
         return
 
