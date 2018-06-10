09a5050442c627f52c0114b4e04a553d24b15950
unknown <gladladvlad@yandex.com>
2018-06-10 12:06:50 +0300

    ~moved searchView(view) to productViews.py

--- server/lib/generalViews.py
+++ server/lib/generalViews.py
@@ -38,34 +38,3 @@ class homepageView(view):
         return content
 
 
-
-class searchView(view):
-    def get(self):
-        debug('[INFO] searchView reached')
-
-
-        self.setContentType('text/html')
-
-        self.addComponentToContext('search_content.html', 'search_content', True)
-        self.addComponentToContext('search_filters.html', 'search_filters', True)
-        self.addComponentToContext('search_styles.html', 'search_styles', True)
-        self.addComponentToContext('navbar.html', 'navbar', True)
-        self.addComponentToContext('footer.html', 'footer', True)
-
-
-        # TODO: get products by filters
-        products = []
-
-        products.append(productController(1, 2, 3, 'bepis', 'abasdkajsd', 5, 6, 7, 8, 9, 10, 11, 12, 13))
-        products.append(productController(1, 2, 3, 'foo', 'hohohohohohoohoohohoohhhhhhh', 5, 6, 7, 8, 9, 10, 11, 12, 13))
-        products.append(productController(1, 2, 3, 'bar', 'merge :hnnn:', 5, 6, 7, 8, 9, 10, 11, 12, 13))
-        # end TODO
-
-
-        #add products to context
-        self.addItemToContext(products, 'products', True)
-
-
-        content = self.renderTemplate('search.html')
-
-        return content
