69feb40f6e99d9168888479cf6038c314c3564be
unknown <gladladvlad@yandex.com>
2018-06-10 20:54:10 +0300

    +completed requestProduct*() functions

--- public/static/js/getProducts_script.js
+++ public/static/js/getProducts_script.js
@@ -3,15 +3,11 @@ var products;
 
 window.onload = function(){
     requestProductIDs();
+    requestProducts(0, 4);
 
     console.log("onload() finished");
 }
 
-/*
-    ('^/search', searchView),
-    ('^/getproductids', searchProductIDsView),
-    ('^/getproducts', searchProductsView)*
-*/
 
 function requestProductIDs(){
     var request = "/getproductids";
@@ -20,28 +16,42 @@ function requestProductIDs(){
     xhr.onreadystatechange = function(){
         if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
             productIDs = JSON.parse(xhr.responseText);
-            alert(productIDs);
-            requestProducts();
         }
     }
 
-    xhr.open("GET", request, true);
+    xhr.open("GET", request, false);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send();
 }
 
-function requestProducts(){
-    var request = "/getproducts?item0=" + productIDs[0].toString();
+function requestProducts(fromIndex, toIndex){
+    if (fromIndex > toIndex) throw "Inverted indexes!";
+    if (fromIndex < 0) throw "Left index lower than 0!";
+    if (toIndex > (productIDs.length - 1)) throw "Right index higher than productIDs.length!";
+
+    var request = "/getproducts";
+
+    request = request + "?";
+    var argIter = "";
+
+    for (var i = fromIndex; i <= toIndex; i++) {
+        argIter = "item" + i.toString() + "=" + productIDs[i].toString();
+
+        request = request + argIter;
+        if (i != toIndex) {
+            request = request + "&";
+        }
+    }
+
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function(){
         if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
             products = JSON.parse(xhr.responseText);
-            alert(products.title);
         }
     }
 
-    xhr.open("GET", request, true);
+    xhr.open("GET", request, false);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send();
 }
