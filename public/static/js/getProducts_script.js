16dd7e825706c4ef6b1e33c0ec5193074dcfdc27
unknown <gladladvlad@yandex.com>
2018-06-11 14:26:15 +0300

    +added getHTTPGArg(key) & updateProductPage(page) to paging script ~fixed pretty much everything in searchPageView & paging script

--- public/static/js/getProducts_script.js
+++ public/static/js/getProducts_script.js
@@ -2,15 +2,47 @@ var productIDs;
 var products;
 var productPage;
 
-window.onload = function(){
-    requestProductIDs();
-    requestProductPage(0, 4);
+var productCountKey = "prods";
 
-    document.write(productPage);
+var queryKey = "query";
+var lastQuery;
+
+var pageSizeKey = "psize";
+var pageSize;
+
+window.onload = function(){
+    lastQuery = getHTTPGArg(queryKey);
+    if (typeof lastQuery == "undefined")
+        lastQuery = "";
+
+    pageSize = getHTTPGArg(pageSizeKey);
+    if (typeof pageSize == "undefined")
+        pageSize = 5;
+    else pageSize = parseInt(pageSize);
+
+    document.getElementById("searchBox").value = lastQuery;
+
+
+    requestProductIDs();
+    updateProductPage(0);
 
     console.log("onload() finished");
 }
 
+
+function updateProductPage(page) {
+    var from = page * pageSize;
+    var to = ((page + 1) * pageSize) - 1;
+    if (to > (productIDs.length - 1)) { 
+        console.log("updateProductPage() firs if: page is " + page);
+        requestProductPage(from, productIDs.length - 1);
+    } else {
+        console.log("updateProductPage() second if: page is " + page);
+        requestProductPage(from, to);
+    }
+
+    document.getElementById("content").innerHTML = productPage;
+}
 
 function requestProductIDs(){
     var request = "/getproductids";
@@ -27,23 +59,30 @@ function requestProductIDs(){
     xhr.send();
 }
 
+
 function requestProductPage(fromIndex, toIndex){
-    if (fromIndex > toIndex) throw "Inverted indexes!";
-    if (fromIndex < 0) throw "Left index lower than 0!";
+    console.log("requestProductPage(); (from, to) = (" + fromIndex + ", " + toIndex + ")");
     if (toIndex > (productIDs.length - 1)) throw "Right index higher than productIDs.length!";
+    if (fromIndex < 0) throw "Left index lower than 0!";
+    if (fromIndex > toIndex) throw "Inverted indexes!";
 
-    var request = "/search_page";
-
-    request = request + "?";
+    var request = "/search_page?" + productCountKey + "=" + productIDs.length + "&" + pageSizeKey + "=" + pageSize + "&";
+    //var request = "/search_page?" + pageSizeKey + "=" + pageSize + "&";
     var argIter = "";
 
+    console.log("request is " + request)
+    console.log("argIter is " + argIter)
+
+    console.log("beginning loop")
     for (var i = fromIndex; i <= toIndex; i++) {
         argIter = "item" + i.toString() + "=" + productIDs[i].toString();
+        console.log("" + i + "th iteration; argIter is " + argIter)
 
         request = request + argIter;
         if (i != toIndex) {
             request = request + "&";
         }
+        console.log("request becomes " + request);
     }
 
     var xhr = new XMLHttpRequest();
@@ -59,6 +98,9 @@ function requestProductPage(fromIndex, toIndex){
     xhr.send();
 }
 
+function getHTTPGArg(key) {
+    return window.location.href.split(new RegExp("[?&]" + key + "="))[1];
+}
 
 /*
 function requestProducts(fromIndex, toIndex){
--- server/lib/productViews.py
+++ server/lib/productViews.py
@@ -1,6 +1,7 @@
 from view import *
 from product import *
 import json
+import math
 
 
 class createListingView(view):
@@ -17,14 +18,14 @@ class createListingView(view):
         return content
 
 
-#searchProductCountKey = 'productCount'
+searchProductCountKey = 'prods'
 searchPageIndexKey = 'page'
 searchPageSizeKey = 'psize'
 searchQueryKey = 'query'
 
 searchDefaultPageSize = 5
 
-productCount = 9
+productCount = 14
 
 class searchView(view):
     def get(self):
@@ -58,10 +59,11 @@ class searchProductIDsView(view):
         # TODO: get product IDs based on query & filters
         productIDs = []
 
-        for iter in xrange(1, productCount):
+        for iter in xrange(0, productCount):
             productIDs.append((((iter * 2 - 1) * 3) / 2) * 10 + iter * 2)
 
         # end TODO
+
 
         return json.dumps(productIDs)
 
@@ -92,6 +94,8 @@ class searchProductsView(view):
             itemKey = 'item{0}'.format(productIter)
         # end TODO
 
+
+
         return json.dumps(products)
 ####################################################################################
 
@@ -103,23 +107,43 @@ class searchPageView(view):
         if not self.urlArgs.has_key(searchPageSizeKey):
             self.urlArgs[searchPageSizeKey] = searchDefaultPageSize
 
+        if not self.urlArgs.has_key(searchProductCountKey):
+            raise ValueError("Did not receive product count!")
+
         self.setContentType('text/html')
 
 
         products = []
 
         productIter = 0
+        productsDone = 0
         itemKey = 'item{0}'.format(productIter)
 
-        while self.urlArgs.has_key(itemKey):
-            # TODO: get products based on query & filters
-            tmpProduct = product(1, 2, self.urlArgs[itemKey], 'title', 'product description lorem gipsum gaudeamus igitur', 4, 5, 6, 7, 8, 9, 10, 11, 12)
 
-            products.append(tmpProduct.asDict())
+        #while self.urlArgs.has_key(itemKey):
+        for i in xrange(0, (int(self.urlArgs[searchProductCountKey]) - 1)):
+            if self.urlArgs.has_key(itemKey):
+                # TODO: get products based on query & filters
+                tmpProduct = product(1, 2, self.urlArgs[itemKey], 'title', 'product description lorem gipsum gaudeamus igitur', 4, 5, 6, 7, 8, 9, 10, 11, 12)
+                # end TODO
+                products.append(tmpProduct.asDict())
+                productsDone += 1
+
 
             productIter += 1
             itemKey = 'item{0}'.format(productIter)
-        # end TODO
+            if (productsDone == int(self.urlArgs[searchPageSizeKey])):
+                break
+
+
+        pages = [dict()]
+
+        for i in xrange(0, int(math.ceil((float(self.urlArgs[searchProductCountKey]) / float(self.urlArgs[searchPageSizeKey]))))):
+            dicterator = {'index' : i}
+
+            pages.append(dicterator)
+
+        self.addItemToContext(pages, 'pages', True)
 
 
         #self.addComponentToContext('search_content.html', 'search_content', True)
