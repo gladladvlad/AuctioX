b9b2f5a48f1833ed28f9fef1f0d30aed6a28a466
RMan135 <razvan.baisan@yahoo.com>
2018-06-12 17:39:07 +0300

    +registration

--- public/static/js/userRegistration_script.js
+++ public/static/js/userRegistration_script.js
@@ -1,3 +1,58 @@
 function sendUserRegisterRequest() {
-    "sendUserRegisterRequest()"
+    console.log("sendUserRegisterRequest()")
+
+    inputEmail = document.getElementById("inputEmail");
+    inputUsername = document.getElementById("inputUsername");
+    inputPassword = document.getElementById("inputPassword");
+    inputConfirmPassword = document.getElementById("inputConfirmPassword");
+
+    inputCountry = document.getElementById("inputCountry");
+    inputState = document.getElementById("inputState");
+    inputCity = document.getElementById("inputCity");
+    inputAddress1 = document.getElementById("inputAddress1");
+    inputAddress2 = document.getElementById("inputAddress2");
+    inputZipCode = document.getElementById("inputZipCode");
+
+    inputName = document.getElementById("inputName");
+    inputTel = document.getElementById("inputTel");
+
+    data = []
+
+    data['email'] = inputEmail.value
+    data['username'] = inputUsername.value
+    data['password'] = inputPassword.value
+    data['confirmPassword'] = inputConfirmPassword.value
+    data['country'] = inputCountry.value
+    data['state'] = inputState.value
+    data['city'] = inputCity.value
+    data['address1'] = inputAddress1.value
+    data['address2'] = inputAddress2.value
+    data['zipCode'] = inputZipCode.value
+    data['name'] = inputName.value
+    data['tel'] = inputTel.value
+
+    
+
+    errorField = document.getElementById("errors")
+    errorField.innerHTML = ""
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function()
+    {
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+            console.log(xhr.responseText)
+            return
+            response = JSON.parse(xhr.responseText)
+            for(er in response["errors"]) {
+                errorField.innerHTML += er + "<br>"
+            }
+            console.log(response["success"])
+            return
+        }
+    }
+
+    xhr.open("POST", "/registrationrequest", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(formData);
 }
