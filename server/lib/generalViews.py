e5464911e933aa0bb77d7237b17b27e95a3600d3
Yee <adrian01@protonmail.com>
2018-06-18 16:51:31 +0300

    Merge branch 'master' of https://github.com/gladladvlad/AuctioX Homepage functionality
    # Conflicts:
    #	server/lib/dispatcherMap.py

--- server/lib/generalViews.py
+++ server/lib/generalViews.py
@@ -27,11 +27,19 @@ class homepageView(view):
 
         self.setContentType('text/html')
 
+        products = []
+        products = productController.getProductsByFilter(None, "date_added", "desc", "")
+        products = productController.getProductImages(products)
+        products = products[0:13]
+        category = "All products"
+
         self.addComponentToContext('home_styles.html', 'style', True)
         self.addComponentToContext('navbar.html', 'navbar', True)
-        self.addComponentToContext('home_content.html', 'content', True)
         self.addComponentToContext('home_homebar.html', 'homebar', True)
         self.addComponentToContext('footer.html', 'footer', True)
+
+        self.addItemToContext(category, 'category', True)
+        self.addItemToContext(products, 'products', True)
 
         content = self.renderTemplate('home.html')
 
